9a9980d3ae2e9a6b35c1cf7477c08272abd43e04
ilyabo <ilyabo@gmail.com>
2020-05-22 19:43:39 +0200

    Not skipping Home page

--- src/App.tsx
+++ src/App.tsx
@@ -61,7 +61,7 @@ function App() {
             <PublicRoute path="/welcome" component={Welcome} />
             {/*<PublicRoute path="/createaccount" component={CreateAccount} />*/}
             {/*<PublicRoute path="/login" component={LogIn} />*/}
-            <PrivateRoute path="/home" component={() => <QuestionnaireCheck><Home/></QuestionnaireCheck>} />
+            <PrivateRoute path="/home" component={Home} />
             <PrivateRoute path="/questionnaire" component={() => <QuestionnaireCheck><Questionnaire/></QuestionnaireCheck>} />
             <PrivateRoute path="/next-video" component={NextVideo} />
           </Switch>
